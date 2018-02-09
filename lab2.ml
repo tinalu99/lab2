d757e180cefec22f8ebf48e7dfe117d7dd2fdee0
Tina Lu <tinalu@MacBook-Pro-5.local>
2018-02-09 00:25:20 -0500

    updated lab2

--- lab2.ml
+++ lab2.ml
@@ -180,11 +180,11 @@ Exercise 8: Now rewrite min_option and max_option using the higher-order
 function calc_option. Call them min_option_2 and max_option_2.
 ......................................................................*)
 
-let min_option_2 =
-  calc_option min ;;
+let min_option_2 (x : int option) (y : int option) : int option =
+  calc_option min x y ;;
 
-let max_option_2 =
-  calc_option max ;;
+let min_option_2 (x : int option) (y : int option) : int option =
+  calc_option max x y ;;
 
 (*......................................................................
 Exercise 9: Now that we have calc_option, we can use it in other
